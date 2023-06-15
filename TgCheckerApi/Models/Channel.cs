744d2255941d66ec917407fc28385b064d9cb814
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-06-15 13:44:04 +0300

    Add project files.

---
+++ TgCheckerApi/Models/Channel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TgCheckerApi.Models;
+
+public partial class Channel
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? Members { get; set; }
+
+    public byte[]? Avatar { get; set; }
+
+    public int? User { get; set; }
+}
---
+++ TgCheckerApi/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using TgCheckerApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TgCheckerDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("MainConnectionString")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.WithOrigins("https://localhost:7007")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("AllowSpecificOrigin");
+
+app.Run();
