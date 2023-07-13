264aa0a855220469a2016bca0f3be19285d72743
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-07-13 23:21:26 +0300

    berebra

--- TgCheckerApi/Controllers/MessageController.cs
+++ TgCheckerApi/Controllers/MessageController.cs
@@ -19,6 +19,12 @@ namespace TgCheckerApi.Controllers
 
         public class SendMessagePayload
         {
+            public string Username { get; set; }
+            public int UserId { get; set; }
+        }
+
+        public class SendMessageResponse
+        {
             public string Token { get; set; }
             public string Username { get; set; }
             public int UserId { get; set; }
@@ -27,8 +33,85 @@ namespace TgCheckerApi.Controllers
         [HttpPost]
         public async Task<IActionResult> SendMessage([FromQuery] string connectionId, [FromBody] SendMessagePayload payload)
         {
-            await _hubContext.Clients.Client(connectionId).SendAsync("ReceiveMessage", JsonConvert.SerializeObject(payload));
+            string token = GenerateUniqueToken();
+
+            SendMessageResponse response = new SendMessageResponse()
+            {
+                Token = token,
+                Username = payload.Username,
+                UserId = payload.UserId
+            };
+
+            await _hubContext.Clients.Client(connectionId).SendAsync("ReceiveMessage", JsonConvert.SerializeObject(response));
             return Ok();
+        }
+
+        public class TokenPayload
+        {
+            public string EncryptedToken { get; set; }
+        }
+
+        [HttpPost("DecryptToken")]
+        public IActionResult DecryptToken([FromBody] TokenPayload payload)
+        {
+            try
+            {
+                Tuple<string, string, string> decryptedToken = Encryptor.DecryptToken(payload.EncryptedToken);
+
+                return Ok(new
+                {
+                    UserId = decryptedToken.Item2,
+                    Username = decryptedToken.Item3
+                });
+            }
+            catch (Exception ex)
+            {
+                // Handle decryption error
+                return BadRequest("Invalid token");
+            }
+        }
+
+        private string GenerateUniqueToken()
+        {
+            string token = GenerateRandomToken();
+
+            // Check if the generated token is unique
+            while (!IsTokenUnique(token))
+            {
+                // If the token is not unique, generate a new one
+                token = GenerateRandomToken();
+            }
+
+            return token;
+        }
+
+        private string GenerateRandomToken()
+        {
+            // Generate a random string using a combination of characters or numbers
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            char[] tokenChars = new char[32];
+
+            for (int i = 0; i < tokenChars.Length; i++)
+            {
+                tokenChars[i] = characters[random.Next(characters.Length)];
+            }
+
+            return new string(tokenChars);
+        }
+
+        private bool IsTokenUnique(string token)
+        {
+            // Check if the token already exists in your system
+            // Implement your logic to verify the uniqueness of the token
+            // You might need to query your database or use another method
+            // to ensure the generated token is unique.
+
+            // Example check (pseudo-code):
+            // return !_tokenRepository.Exists(token);
+
+            // Make sure to replace the above example with your own implementation.
+            return true; // Change this line accordingly
         }
     }
 }
---
+++ TgCheckerApi/Encryptor.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TgCheckerApi
+{
+    public static class Encryptor
+    {
+        private static readonly byte[] EncryptionKey = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10 };
+
+        public static string EncryptToken(string token, string userId, string username)
+        {
+            byte[] encryptedBytes;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = EncryptionKey;
+                aes.GenerateIV();
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                byte[] tokenBytes = Encoding.UTF8.GetBytes(token);
+                byte[] userIdBytes = Encoding.UTF8.GetBytes(userId);
+                byte[] usernameBytes = Encoding.UTF8.GetBytes(username);
+
+                byte[] encryptedTokenBytes = encryptor.TransformFinalBlock(tokenBytes, 0, tokenBytes.Length);
+                byte[] encryptedUserIdBytes = encryptor.TransformFinalBlock(userIdBytes, 0, userIdBytes.Length);
+                byte[] encryptedUsernameBytes = encryptor.TransformFinalBlock(usernameBytes, 0, usernameBytes.Length);
+
+                encryptedBytes = CombineArrays(aes.IV, encryptedTokenBytes, encryptedUserIdBytes, encryptedUsernameBytes);
+            }
+
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public static Tuple<string, string, string> DecryptToken(string encryptedToken)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedToken);
+            byte[] iv = new byte[16];
+            byte[] encryptedTokenBytes = new byte[encryptedBytes.Length - 48];
+            byte[] encryptedUserIdBytes = new byte[16];
+            byte[] encryptedUsernameBytes = new byte[16];
+
+            Buffer.BlockCopy(encryptedBytes, 0, iv, 0, 16);
+            Buffer.BlockCopy(encryptedBytes, 16, encryptedTokenBytes, 0, encryptedTokenBytes.Length);
+            Buffer.BlockCopy(encryptedBytes, 16 + encryptedTokenBytes.Length, encryptedUserIdBytes, 0, 16);
+            Buffer.BlockCopy(encryptedBytes, 32 + encryptedTokenBytes.Length, encryptedUsernameBytes, 0, 16);
+
+            string token;
+            string userId;
+            string username;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = EncryptionKey;
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                byte[] decryptedTokenBytes = decryptor.TransformFinalBlock(encryptedTokenBytes, 0, encryptedTokenBytes.Length);
+                byte[] decryptedUserIdBytes = decryptor.TransformFinalBlock(encryptedUserIdBytes, 0, encryptedUserIdBytes.Length);
+                byte[] decryptedUsernameBytes = decryptor.TransformFinalBlock(encryptedUsernameBytes, 0, encryptedUsernameBytes.Length);
+
+                token = Encoding.UTF8.GetString(decryptedTokenBytes);
+                userId = Encoding.UTF8.GetString(decryptedUserIdBytes);
+                username = Encoding.UTF8.GetString(decryptedUsernameBytes);
+            }
+
+            return new Tuple<string, string, string>(token, userId, username);
+        }
+
+        private static byte[] CombineArrays(params byte[][] arrays)
+        {
+            int totalLength = arrays.Sum(a => a.Length);
+            byte[] combinedArray = new byte[totalLength];
+
+            int currentIndex = 0;
+            foreach (byte[] array in arrays)
+            {
+                Buffer.BlockCopy(array, 0, combinedArray, currentIndex, array.Length);
+                currentIndex += array.Length;
+            }
+
+            return combinedArray;
+        }
+    }
+}
