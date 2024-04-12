f1967df968632f01c5611f9d02e64abfd9795f42
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2024-04-12 21:29:29 +0300

    амогус

--- TgCheckerApi/Controllers/TelegramController.cs
+++ TgCheckerApi/Controllers/TelegramController.cs
@@ -11,6 +11,7 @@ using System.Text.Json;
 using System.Security.Cryptography.Xml;
 using Newtonsoft.Json;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace TgCheckerApi.Controllers
 {
@@ -177,6 +178,108 @@ namespace TgCheckerApi.Controllers
             {
                 return StatusCode(500, $"An error occurred: {ex.Message}");
             }
+        }
+
+        class Porter
+        {
+
+
+
+            private const string VOWEL = "аеиоуыэюя";
+
+            private const string PERFECTIVEGROUND = "((ив|ивши|ившись|ыв|ывши|ывшись)|((?<=[ая])(в|вши|вшись)))$";
+
+            private const string REFLEXIVE = "(с[яь])$";
+
+            private const string ADJECTIVE = "(ее|ие|ые|ое|ими|ыми|ей|ий|ый|ой|ем|им|ым|ом|его|ого|еых|ую|юю|ая|яя|ою|ею)$";
+
+            private const string PARTICIPLE = "((ивш|ывш|ующ)|((?<=[ая])(ем|нн|вш|ющ|щ)))$";
+
+            private const string VERB = "((ила|ыла|ена|ейте|уйте|ите|или|ыли|ей|уй|ил|ыл|им|ым|ены|ить|ыть|ишь|ую|ю)|((?<=[ая])(ла|на|ете|йте|ли|й|л|ем|н|ло|но|ет|ют|ны|ть|ешь|нно)))$";
+
+            private const string NOUN = "(а|ев|ов|ие|ье|е|иями|ями|ами|еи|ии|и|ией|ей|ой|ий|й|и|ы|ь|ию|ью|ю|ия|ья|я)$";
+
+            private const string RVRE = "^(.*?[аеиоуыэюя])(.*)$";
+
+            private const string DERIVATIONAL = "[^аеиоуыэюя][аеиоуыэюя]+[^аеиоуыэюя]+[аеиоуыэюя].*(?<=о)сть?$";
+
+            private const string SUPERLATIVE = "(ейше|ейш)?";
+
+
+            public string Stemm(string word)
+            {
+                word = word.ToLower();
+                word = word.Replace("ё", "е");
+                if (IsMatch(word, RVRE))
+                {
+
+                    if (!Replace(ref word, PERFECTIVEGROUND, ""))
+                    {
+                        Replace(ref word, REFLEXIVE, "");
+                        if (Replace(ref word, ADJECTIVE, ""))
+                        {
+                            Replace(ref word, PARTICIPLE, "");
+                        }
+                        else
+                        {
+                            if (!Replace(ref word, VERB, ""))
+                            {
+                                Replace(ref word, NOUN, "");
+                            }
+
+                        }
+
+                    }
+
+
+                    Replace(ref word, "и$", "");
+
+                    if (IsMatch(word, DERIVATIONAL))
+                    {
+                        Replace(ref word, "ость?$", "");
+                    }
+
+
+                    if (!Replace(ref word, "ь$", ""))
+                    {
+                        Replace(ref word, SUPERLATIVE, "");
+                        Replace(ref word, "нн$", "н");
+                    }
+
+                }
+
+                return word;
+            }
+
+            private bool IsMatch(string word, string matchingPattern)
+            {
+                return new Regex(matchingPattern).IsMatch(word);
+            }
+
+            private bool Replace(ref string replace, string cleaningPattern, string by)
+            {
+                string original = replace;
+                replace = new Regex(cleaningPattern,
+                            RegexOptions.ExplicitCapture |
+                            RegexOptions.Singleline
+                            ).Replace(replace, by);
+                return original != replace;
+            }
+
+        }
+
+        [HttpPost("GetRoot")]
+        public ActionResult<string> GetRoot(string word)
+        {
+            if (word == null || string.IsNullOrWhiteSpace(word))
+            {
+                return BadRequest("Please provide a valid word.");
+            }
+
+            var stemmer = new Porter();
+            var root = stemmer.Stemm(word);
+
+            return Ok(root);
         }
     }
 }
